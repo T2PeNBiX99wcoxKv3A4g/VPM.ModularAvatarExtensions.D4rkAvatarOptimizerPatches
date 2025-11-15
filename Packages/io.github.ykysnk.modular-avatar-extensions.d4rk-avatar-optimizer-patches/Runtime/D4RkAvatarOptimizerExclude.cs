eec41bfda87d8b2371e3e91a4bbcee7ee89a0e8a
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-15 16:32:32 +0800

    fix: Update base class in D4RkAvatarOptimizerExclude to AvatarMaexComponent

--- Packages/io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches/Runtime/D4RkAvatarOptimizerExclude.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches/Runtime/D4RkAvatarOptimizerExclude.cs
@@ -1,11 +1,10 @@
-using io.github.ykysnk.utils;
 using UnityEngine;
 
 namespace io.github.ykysnk.ModularAvatarExtensions
 {
     [DisallowMultipleComponent]
     [AddComponentMenu("Modular Avatar EX/MAEX d4rk Avatar Optimizer Exclude")]
-    public class D4RkAvatarOptimizerExclude : YkyEditorComponent
+    public class D4RkAvatarOptimizerExclude : AvatarMaexComponent
     {
     }
 }
