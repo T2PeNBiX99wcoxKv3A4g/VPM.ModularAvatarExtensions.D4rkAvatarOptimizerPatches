b886bf65a3ebeae486e9af64d1e4e2f275d5da3f
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-15 16:34:34 +0800

    fix: Update base class and method in D4RkAvatarOptimizerExcludeEditor

--- Packages/io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches/Editor/D4RkAvatarOptimizerExcludeEditor.cs
+++ Packages/io.github.ykysnk.modular-avatar-extensions.d4rk-avatar-optimizer-patches/Editor/D4RkAvatarOptimizerExcludeEditor.cs
@@ -3,16 +3,11 @@ using UnityEditor;
 namespace io.github.ykysnk.ModularAvatarExtensions.Editor;
 
 [CustomEditor(typeof(D4RkAvatarOptimizerExclude))]
-public class D4RkAvatarOptimizerExcludeEditor : UnityEditor.Editor
+[CanEditMultipleObjects]
+public class D4RkAvatarOptimizerExcludeEditor : MaexEditor
 {
-    public override void OnInspectorGUI()
+    protected override void OnInspectorGUIDraw()
     {
-        serializedObject.Update();
-        EditorGUI.BeginChangeCheck();
-
         EditorGUILayout.HelpBox("This object will be exclude in d4rk Avatar Optimizer", MessageType.Info, true);
-
-        if (EditorGUI.EndChangeCheck())
-            serializedObject.ApplyModifiedProperties();
     }
 }
